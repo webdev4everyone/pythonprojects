c4906d48ce111995682934984acd72e2376162ea
webdev4everyone <71544885+webdev4everyone@users.noreply.github.com>
2020-09-30 18:54:03 +0530

    Create comparenumbers.py

---
+++ comparenumbers.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+if num1 > num2:
+    print("Number 1 is greater")
+elif num2 > num1:
+    print("Number 2 is greater")
+else:
+    print("Numbers are equal")
