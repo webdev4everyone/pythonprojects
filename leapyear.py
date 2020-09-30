e8bc44e8bfd6bb3ae9964300cb823326f6f2f257
webdev4everyone <71544885+webdev4everyone@users.noreply.github.com>
2020-09-30 18:47:16 +0530

    Create leapyear.py
    Leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+year1 = 2019 # Change 2019 to the year you wish to be the first
+year2 = 2024 # Change 2024 to the year you wish to be the first
+if year1 % 4 == 0:
+    print("Year 1 is a leap year") # If year 1 is div by 4 then it is a leap year.
+else:
+    print("Year 1 is not a leap year") # If year 1 is not div by 4 then it is not a leap year.
+if year2 % 4 == 0:
+    print("Year 2 is a leap year") # If year 2 is div by 4 then it is a leap year.
+else:
+    print("Year 2 is not a leap year") # If year 2 is not div by 4 then it is not a leap year.
+
